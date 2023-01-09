5853803a11b5a7d14d39d9b50a412f08731f0230
kishore-mukh <76032404+kishore-mukh@users.noreply.github.com>
2023-01-09 14:14:03 +0530

    commit changes

---
+++ MyFolder/NewClass.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyFolder
+{
+    public class NewClass
+    {
+        //HOla no dore
+    }
+}
